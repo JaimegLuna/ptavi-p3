bacf70ee51f79fd26b30d3f7ef41cfbf91336b65
Jaime Garcia Luna <jluna@gamma26.aulas.gsyc.urjc.es>
2016-10-03 15:57:06 +0200

    Practica3 incompleta

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -1,4 +1,48 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from xml.sax import make_parser 
+from xml.sax.handler import ContentHandler
 
+
+class SmallSMILHandler(ContentHandler):
+
+
+	def __init__(self):
+		"""
+        Constructor. Inicializamos las variables
+        """
+		self.etiquetas={}
+		self.lista = {'root-layout':['width', 'height', 'background-color'], 
+					'region':['id', 'top', 'bottom', 'left', 'right'],
+					'img':['src', 'region', 'begin', 'dur'],
+					'audio':['src', 'begin', 'dur'],
+					'textstream':['src', 'region']}
+		self.atr{}
+
+	def startElement(self, name, attrs):
+		"""
+        Método que se llama cuando se abre una etiqueta
+        """
+		if name == 'self.lista': 
+		#Asi cogemos todos los atributos de mi diccionario y no tengo que hacer varios if
+			for name in self.etiquetas: #iteramos por el diccionario 
+				self.atr = attrs.get('valor', "")
+
+	def endElement( self, name)
+				
+		
+
+
+
+
+
+		
+if __name__=="__main__":
+	 """
+    Programa principal
+    """
+    parser = make_parser()
+    cHandler = ChistesHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open('chistes2.xml'))
