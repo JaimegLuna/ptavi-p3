3dd2cd6c162f10d1bb068572bb69f18b76d477e3
Jaime Garcia Luna <jluna@gamma37.aulas.gsyc.urjc.es>
2016-10-10 15:54:56 +0200

    casi karaoke

--- karaoke.py
+++ karaoke.py
@@ -7,29 +7,42 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
  
             
-    def new_lista(fichero):
-        parser = make_parser()
-        cHandler = SmallSMILHandler()
-        parser.setContentHandler(cHandler)
-        parser.parse(fichero)
-        lista=cHandler.get_tags()
-        return lista
+def new_list(archivo):
+    parser = make_parser()
+    cHandler = SmallSMILHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(archivo)
+    list=cHandler.get_tags()
+    return list       
 
-    def print_lista(lista)
-        
-    
-    def open_file():
-        try:
-            fichero=open (sys.argv[1],'r')
-            return fichero
-        except IndexError:  
-            sys.exit("Usage: python3 karaoke.py file.smil")
-    
-  
+def pass_list(lista):
+    linea = ""
+    for elemento in lista:
+        linea = linea + elemento[0]
+        atr= elemento[1].dato()
+        for name, atributo in atr:
+            linea = linea + '\t' + name + ' = ' + '"' + atributo + '"'
+        linea = linea + '\n'
 
+    print (linea)
 
+def open_file():
+    try:
+        archivo=open(sys.argv[1],'r')
+        return archivo
+    except IndexError:  
+        sys.exit("Usage: python3 karaoke.py file.smil")
 
-if __name__=="main__":
+if __name__=="__main__":
+
+    fichero = open_file()
+    parser = make_parser()
+    cHandler = SmallSMILHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(fichero)
+    print(cHandler.get_tags())
+    lista = new_list(fichero)
+    pass_lista(lista)
 
     
 
