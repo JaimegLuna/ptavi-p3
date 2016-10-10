05d67b1b8dab4cb6a171980493b363ae964e137c
Jaime Garcia Luna <jluna@gamma37.aulas.gsyc.urjc.es>
2016-10-10 12:03:52 +0200

    smallsmilhandler completo

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -8,55 +8,46 @@ from xml.sax.handler import ContentHandler
 class SmallSMILHandler(ContentHandler):
 
     def __init__(self):
-		"""
-        Constructor. Inicializamos las variables
-        """
 
         self.etiquetas = []
 
         self.lista = {'root-layout':['width', 'height', 'background-color'], 
-					'region':['id', 'top', 'bottom', 'left', 'right'],
-					'img':['src', 'region', 'begin', 'dur'],
-					'audio':['src', 'begin', 'dur'],
-					'textstream':['src', 'region']}
+                    'region':['id', 'top', 'bottom', 'left', 'right'],
+                    'img':['src', 'region', 'begin', 'dur'],
+                    'audio':['src', 'begin', 'dur'],
+                    'textstream':['src', 'region']}
 
-		self.atr={}
-
-	def startElement(self, name, attrs):
-		"""
+    def startElement(self, name, attrs):
+        """
         Método que se llama cuando se abre una etiqueta
         """
-		if name == 'self.lista': 
-		#Asi cogemos todos los atributos de mi diccionario y no tengo que hacer varios if
-			for name in self.etiquetas: #iteramos por el diccionario 
-				self.atr = attrs.get('valor', "")
-            self.nueva_lista(name, slef.atributo)
-	
-	def get_tags(self):
-		return self.etiquetas
+        if name in self.lista: 
+        #Asi cogemos todos los atributos de mi diccionario y no tengo que hacer varios if
+        #iteramos por el diccionario 
+            self.atr={}
+            for valor in self.lista[name]:
+                self.atr[valor] = attrs.get(valor, "")
+            self.nueva_lista(name, self.atr)
 
-	def nueva_lista(self, nombre, atributos):
+    
+    def get_tags(self):
+        return self.etiquetas
+
+    def nueva_lista(self, nombre, atr):
         etiqueta=[]
-        self.etiqueta= self.endetiqueta(name)
-		self.etoqueta= self.endetiqueta(atributo)
+        etiqueta.append(nombre)
+        etiqueta.append(atr)
+        self.etiquetas.append(etiqueta)
+        return self.etiquetas
 
-
-	def print()
-		return self.lista 
-        
-		
-		
-
-
-
-
-
-		
+def print_etiqueta(etiquetas):
+    for elementos in etiquetas:
+        print(elementos)
+    
 if __name__=="__main__":
-	 """
-    Programa principal
-    """
+
     parser = make_parser()
-    cHandler = ChistesHandler()
+    cHandler = SmallSMILHandler()
     parser.setContentHandler(cHandler)
-    parser.parse(open('chistes2.xml'))
+    parser.parse(open("karaoke.smil"))
+    print_etiqueta(cHandler.get_tags())
