778dac69509410bb1b4c8eb3253140159334a7a7
Jaime Garcia Luna <jluna@alpha30.aulas.gsyc.urjc.es>
2017-10-16 12:37:19 +0200

    SSH pep8 con problemas

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -4,6 +4,9 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+# No se como solucionar el problema que me da el pep8 (error w191 "Identation contains tabs)
+
+
 class SmallSmillHandler(ContenetHandler):
 	"""
 	Esta clase se utiliza para utilizar un fichero SMIL
@@ -13,9 +16,8 @@ class SmallSmillHandler(ContenetHandler):
 	"""
 	Con esto, inicializamos las variables
 	"""
-		
 		self.lista = []
-		self.dic = {'root-layout': ['width', 'height','background-color'],	
+		self.dic = {'root-layout': ['width', 'height', 'background-color'],	
 			'region': ['id', 'top', 'bottom', 'left', 'right'],
 			'img': ['src', 'region', 'begin', 'dur'],
 			'audio': ['src', 'begin', 'dur'],
@@ -27,7 +29,7 @@ class SmallSmillHandler(ContenetHandler):
 	"""
 	
 		if name in self.dic:
-		#asi vamos escogiendo atributo a atributo de la lista 
+		# asi vamos escogiendo atributo a atributo de la lista 
 			print(name)
 			for atributo in self.dic[name]:
 				self.atrib = {}
@@ -45,6 +47,7 @@ if __name__=="__main__":
 	parser = make_parser()
 	parser.setContentHandler(CHandler)
 	parser.parse(open('karaoke.smil'))
-	#Karaoke.smil es un ejemplo, una prueba de que funciona con smil
+	# Karaoke.smil es un ejemplo, una prueba de que funciona con smil
 	
 	print(CHandler.get_tags())   
+
